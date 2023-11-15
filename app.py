a5447d0494f456e4926156c5044013cdcb0b6015
Atte Pajula <attepajula@yahoo.com>
2023-11-15 17:29:37 +0200

    Login added

--- app.py
+++ app.py
@@ -1,12 +1,68 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
 from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
+from sqlalchemy.sql import text
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        sql = "SELECT user_id, password FROM users WHERE username=:username"
+        result = db.session.execute(text(sql), {"username": username})
+        user = result.fetchone()
+
+        if not user:
+            flash("Virheelliset kirjautumistiedot", "error")
+        else:
+            hash_value = user.password
+            if check_password_hash(hash_value, password):
+                session['username'] = username  # Tallenna käyttäjänimi sessioon
+                flash("Kirjautuminen onnistui", "success")
+            else:
+                flash("Virheelliset kirjautumistiedot", "error")
+
+    return redirect("/")
+
+@app.route("/add_new_user", methods=["POST"])
+def add_new_user():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        hash_value = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        try:
+            db.session.execute(text(sql), {"username": username, "password": hash_value})
+            db.session.commit()
+            flash("Käyttäjä luotu", "success")
+        except:
+            flash("Virhe: käyttäjänimi varattu", "error")
+      
+    return redirect("/signup")
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)  # Poista käyttäjänimi sessiosta
+    return redirect("/")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
