f7ea2eecf8d6317c5cb40d9de97d8229fd2715d5
Atte Pajula <attepajula@yahoo.com>
2023-12-17 16:38:35 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- Drop old tables if they exist
-DROP TABLE IF EXISTS permissions, users, project_inventory_usage, projects, inventories CASCADE;
+DROP TABLE IF EXISTS permissions, users, project_inventory_usage, projects, inventories , materials, material_inventory, project_material_needs CASCADE;
 
 -- Define new tables
 CREATE TABLE IF NOT EXISTS inventories (
@@ -34,6 +34,29 @@ CREATE TABLE IF NOT EXISTS permissions (
     project_owner_name VARCHAR(50) NOT NULL,
     can_modify BOOLEAN
 );
+
+CREATE TABLE IF NOT EXISTS materials (
+    material_id SERIAL PRIMARY KEY,
+    material_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    unit VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS material_inventory (
+    material_inventory_id SERIAL PRIMARY KEY,
+    material_id INTEGER REFERENCES materials(material_id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL,
+    notes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS project_material_needs (
+    project_material_needs_id SERIAL PRIMARY KEY,
+    project_id INTEGER REFERENCES projects(project_id) ON DELETE CASCADE,
+    stage_id INTEGER,
+    material_id INTEGER REFERENCES materials(material_id) ON DELETE CASCADE,
+    quantity_needed INTEGER NOT NULL
+);
+
 
 -- Define a new view
 CREATE OR REPLACE VIEW project_inventory_usage_view AS
